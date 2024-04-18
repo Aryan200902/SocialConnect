2edaa9d92cde378959566336e3e7e763d8ae8650
Aryan Shrivastava <aryanshrivastava987@gmail.com>
2024-04-18 21:20:39 +0530

    build

--- socialconnect/settings.py
+++ socialconnect/settings.py
@@ -74,14 +74,19 @@ WSGI_APPLICATION = 'socialconnect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# database_url = os.getenv("DATABASE_URL")
+# DATABASES['default']=dj_database_url.parse(database_url)
+
+DATABASE_URL = os.getenv('DATABASE_URL')
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(),
 }
-database_url = os.getenv("DATABASE_URL")
-DATABASES['default']=dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
